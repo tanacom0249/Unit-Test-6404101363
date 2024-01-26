9709a0991d82f68a8cc767e274903499dad1eb5e
tanacom0249 <119926673+tanacom0249@users.noreply.github.com>
2024-01-26 11:25:17 +0700

    quasar-test-trigger-click-button

--- test/jest/__tests__/demo/IndexPage.spec.js
+++ test/jest/__tests__/demo/IndexPage.spec.js
@@ -15,3 +15,10 @@ test('should show the form element on the user output', () => {
     const wrapper = shallowMount(InputPage)
     expect(wrapper.find('button').exists()).toBe(true)
   })
+
+  test('trigger click event on button ', async () => {
+    const wrapper = shallowMount(InputPage)
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('submit form')
+  })
