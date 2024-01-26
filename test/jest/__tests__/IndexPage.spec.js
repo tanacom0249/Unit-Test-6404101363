c9d325dce392bab695538c8b9c385d968ae9b584
tanacom0249 <119926673+tanacom0249@users.noreply.github.com>
2024-01-26 10:46:02 +0700

    puasar-test--title

--- test/jest/__tests__/IndexPage.spec.js
+++ test/jest/__tests__/IndexPage.spec.js
@@ -9,4 +9,18 @@ describe('IndexPage', () => {
     expect(header.text())
       .toBe('Vue is awesome.')
   })
+
+  it('check text content to be as defined in variable', () => {
+    const wrapper = shallowMount(IndexPage, {
+      data () {
+        return {
+          title: 'I love Vue.'
+        }
+      }
+    })
+    let header = wrapper.find('.htmlClass h1')
+    expect(header.text()).toBe('I love Vue.')
+  })
+
 })
+
